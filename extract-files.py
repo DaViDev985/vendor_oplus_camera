b2ac96278c01e0ce46770c91e947825ac165f7b6
realahnet <syedmusicrealise@gmail.com>
2026-06-28 14:11:38 +0500

    camera: Drop oppogallery
    * The cam will now allow users to select a gallery app of their own, like any other camera app. We dont need more bloat on AOSP roms.

--- extract-files.py
+++ extract-files.py
@@ -59,8 +59,6 @@ blob_fixups = {
         .apktool_patch('patches'),
     'system_ext/framework/com.oplus.camera.unit.sdk.jar': blob_fixup()
         .apktool_patch('patches-sdk'),
-    'system_ext/priv-app/OppoGallery2/OppoGallery2.apk': blob_fixup()
-        .apktool_patch('patches-gallery'),
     'system_ext/priv-app/OplusAppPlatform/OplusAppPlatform.apk': blob_fixup()
         .apktool_patch('patches-appplatform'),
     'odm/etc/init/init.camera_process.rc': blob_fixup()
