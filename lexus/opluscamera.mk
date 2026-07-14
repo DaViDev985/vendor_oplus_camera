67fb19a84dfd98f664d38869d277c82f1c54a82c
DaViDev985 <davidev985@gmail.com>
2026-07-14 18:07:00 +0200

    Revert "camera: Drop oppogallery"
    This reverts commit b2ac96278c01e0ce46770c91e947825ac165f7b6.

--- lexus/opluscamera.mk
+++ lexus/opluscamera.mk
@@ -40,8 +40,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.logd.log.load.vendor.qti.camera.provider-service_64.upper_limit=1500 \
 
 # Camera
-$(call soong_config_set,camera,target_ships_oplus_cam,true)
-$(call soong_config_set,cameraservice,allow_nonincreasing_timestamps,true)
+$(call soong_config_set_bool,camera,target_ships_oplus_cam,true)
+$(call soong_config_set_bool,cameraservice,allow_nonincreasing_timestamps,true)
 $(call soong_config_set,libgui,num_buffer_slots,96)
 
 # OPlus FWK
